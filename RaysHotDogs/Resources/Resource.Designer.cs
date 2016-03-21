1d40c48d3b23869ec6d67915b44858ea6e04b3fb
Adrian Limon <me@adrianlimon.com>
2016-03-20 20:50:47 -0700

    added hot dog detail view

--- RaysHotDogs/Resources/Resource.Designer.cs
+++ RaysHotDogs/Resources/Resource.Designer.cs
@@ -60,8 +60,35 @@ namespace RaysHotDogs
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050009
+			public const int MyButton = 2131034121;
+			
+			// aapt resource value: 0x7f050007
+			public const int amountEditText = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int cancelButton = 2131034118;
+			
+			// aapt resource value: 0x7f050004
+			public const int descriptionTextView = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int hotDogImageView = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int hotDogNameTextView = 2131034114;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int mainScrollView = 2131034112;
+			
+			// aapt resource value: 0x7f050008
+			public const int orderButton = 2131034120;
+			
+			// aapt resource value: 0x7f050005
+			public const int priceTextView = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int shortDescriptionTextView = 2131034115;
 			
 			static Id()
 			{
@@ -77,7 +104,10 @@ namespace RaysHotDogs
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int HotDogDetailView = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
